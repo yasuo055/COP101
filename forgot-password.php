009923cf9e1a99e175271250f64b98bb7d45d66f
enlowjap <145821911+enlowjap@users.noreply.github.com>
2024-11-26 02:00:52 +0800

    Transfer html to php and add config file for database
    login,signup,forget-password to php file, added database sql and config file for database connection.

---
+++ forgot-password.php
@@ -0,0 +1,67 @@
+<?php 
+include('Conn.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="/icon/PONDTECH__2_-removebg-preview 2.png">
+  <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <title>Aqua Sense</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="cont">
+    <div class="left-por-log">
+      <img src="/asset/image 15.png" class="img-left-log">
+    </div>
+    
+    <div class="right-por-log">
+      <div class="head-log">
+        <img src="/icon/PONDTECH__2_-removebg-preview 2.png" class="head-log-sub">
+        <p class="up-head-log-txt">
+          Forgot Password
+        </p>
+        <p class="sub-heading-log">
+          We sent a code to your Phone Number
+        </p>
+      </div>
+
+      <div class="for-pass-aut">
+        <div class="forg-log">
+          <input type="tel" class="for-pass-inp">
+        </div>
+  
+        <div class="forg-log">
+          <input type="tel" class="for-pass-inp">
+        </div>
+  
+        <div class="forg-log">
+          <input type="tel" class="for-pass-inp">
+        </div>
+  
+        <div class="forg-log">
+          <input type="tel" class="for-pass-inp">
+        </div>
+      </div>
+
+      <div class="bottom-log">
+        <a href="/signup.html">
+        <button class="btn-log">
+          Continue
+        </button>
+       </a>
+        <p class="bot-head-log-txt">
+          Didn't recieve a code
+          <a href="signup.html">
+          <span class="sgn-up-log"> Click To Resend </span>
+          </a>
+        </p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
