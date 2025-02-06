8c751e9a8625a012ce749cf9e9264ae8dd0cc67e
Mat <92700247+yasuo055@users.noreply.github.com>
2025-02-06 19:39:28 +0800

    add

---
+++ administrator/Conn.php
@@ -0,0 +1,21 @@
+<?php 
+    //database config
+    $host = 'localhost'; //hostname
+    $dbname ='aquasensedb'; //dbname
+    $username = 'root'; //username
+    $password = ''; //password
+
+    //pdo conn string
+    $conn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+    try{
+        //PDO instance(connect to db)
+        $connpdo = new PDO ($conn,$username,$password);
+
+        //error mode to exception
+        $connpdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e){
+        // Handle Error
+        echo "connection failed: " . $e->getMessage();
+        exit;}
+?>
