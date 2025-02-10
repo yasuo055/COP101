f3dabc2d6c479f86a494989dec0a6f61ad4c9d4e
Mat <92700247+yasuo055@users.noreply.github.com>
2025-02-10 11:59:59 +0800

    update all-user-search

--- administrator/search-all-users.php
+++ administrator/search-all-users.php
@@ -26,10 +26,11 @@ if ($stmt->rowCount() > 0) {
             echo "<td>" . htmlspecialchars($row['DATECREATED']) . "</td>";
             echo "<td>" . htmlspecialchars($row['ROLE']) . "</td>";
             echo "<td>
-                    <a href='edit-user.php?userid=" . $row['USERID'] . "'>
+                  
+    <a href='edit-user.php?userid=" . $row['USERID'] . "'>
             <button class='action-btn edit-btn'>Edit</button>
         </a>
-              <button class='action-btn archive-btn' data-id='" . $row['USERID'] . "'>Archive</button>
+                 <button class='action-btn archive-btn' data-id='" . $row['USERID'] . "'>Archive</button>
 
                   </td>";
             echo "</tr>";
