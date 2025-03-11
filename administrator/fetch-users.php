2c960fe522c21e6d449e2bb932d71f659ef5995f
Mat <92700247+yasuo055@users.noreply.github.com>
2025-03-11 12:59:10 +0800

    update this is fix

--- administrator/fetch-users.php
+++ administrator/fetch-users.php
@@ -18,8 +18,7 @@ $sql = "SELECT
     DATE_FORMAT(DATECREATED, '%Y-%m-%d %r') AS DATECREATED, 
     ROLE 
 FROM users 
-WHERE archived != 1
-ORDER BY DATECREATED DESC";
+WHERE archived != 1";
 
 // Prepare parameters
 $params = [];
@@ -62,7 +61,7 @@ if ($stmt->rowCount() > 0) {
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
         echo "<tr>";
         echo "<td>" . htmlspecialchars($row['USERID']) . "</td>";
-        echo "<td>" . htmlspecialchars($row['FNAME'] . ' ' . $row['MNAME'] . ' ' . $row['LNAME']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['FULLNAME']) . "</td>";
         echo "<td>" . htmlspecialchars($row['USERNAME']) . "</td>";
         echo "<td>" . htmlspecialchars($row['EMAIL']) . "</td>";
         echo "<td>" . htmlspecialchars($row['CONTACT']) . "</td>"; 
